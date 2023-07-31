3443678a99d0740690086d4c68aaefaa56ab7073
brandxl <135657660+brandxl@users.noreply.github.com>
2023-07-31 10:50:49 -0400

    Add files via upload
    adding var vs const vs let acticity

---
+++ varVsConst.js
@@ -0,0 +1,24 @@
+//ES5 Global Constants
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+//ES2015 Global Constants
+const PI = 3.14;
+PI = 42; // Error!
+//Quiz
+//What is the difference between var and let?
+
+//Var allows you to redeclare or reassign, whereas let will not allow this. Also you are able to refer to var hoisted  Let is blocked.
+
+//What is the difference between var and const?
+
+//Var allows you to redeclare or reassign. Const is set and it is also blocked 
+
+//What is the difference between let and const?
+
+//Let allows you to reassign as seen in so many functions, const does not allow for redeclaration or reassignment Const is set
+
+//What is hoisting?
+
+//Hoisting a var like a sail is hoisted ...can be done JS accesses it even though at the top before it is used.
+
+//Functions can also be accessed after hoisted before being used or defined later on 
